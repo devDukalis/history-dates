110e2d980cac460b57caa53ce696419f5a9aa042
devDukalis <devdukalis@gmail.com>
2023-12-11 20:27:38 +0300

    feat: eslint and prettier were setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
