f2c84d8cc030652703661fefd8d4415e9a758ecd
devDukalis <devdukalis@gmail.com>
2023-12-12 14:37:17 +0300

    feat: css reset, fonts, variables were added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 
+import "./scss/min-reset.scss";
+import "./scss/fonts.scss";
+import "./index.scss";
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
